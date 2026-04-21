56c0a52264f0f6144280d5ba5a71422a715d5835
Petro <petropavliuk12.05@gmail.com>
2026-04-21 23:42:57 +0300

    fixed logic issues

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class CarWashStation:
         distance_from_city_center: int | float,
         clean_power: int,
         average_rating: int | float,
-        count_of_ratings: int
+        count_of_ratings: int,
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -25,24 +25,27 @@ class CarWashStation:
     def calculate_washing_price(self, car: object) -> float:
         difference = self.clean_power - car.clean_mark
         if difference <= 0:
-            return 0
-        return (
-            car.comfort_class
-            * difference
-            * self.average_rating
-            / self.distance_from_city_center
+            return 0.0
+        return round(
+            (
+                car.comfort_class
+                * difference
+                * self.average_rating
+                / self.distance_from_city_center
+            ),
+            1,
         )
 
     def wash_single_car(self, car: object) -> object:
-        if car.clean_mark <= self.clean_power:
+        if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
-            car.clean_mark <= self.clean_power
-            price = self.calculate_washing_price(car)
-            income += price
+            if car.clean_mark < self.clean_power:
+                price = self.calculate_washing_price(car)
+                income += price
             self.wash_single_car(car)
         return round(income, 1)
 
